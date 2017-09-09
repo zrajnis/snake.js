da6c2012d5d5deae19440a93d507df7daa91c2fc
zrajnis <zvone.rajnis@gmail.com>
2017-09-09 18:59:24 +0200

    Snake, the game.

---
+++ snake.js
@@ -0,0 +1,154 @@
+const c = document.getElementById('gameCanvas');
+const cellSize = 10;
+const ctx = c.getContext('2d');
+const food = {};
+const highScore = document.getElementById('highScore');
+const highScoreContainer = document.getElementById('highScoreContainer');
+const snake = {};
+
+food.draw = function () {
+  drawSquare(this.x, this.y, 'green', 'green');
+};
+
+food.setPosition = function () {
+  this.x = Math.round(Math.random() * (c.width - cellSize) / cellSize);
+  this.y = Math.round(Math.random() * (c.height - cellSize) / cellSize);
+};
+
+snake.checkCollision = function () {
+  const head = this.cells[0];
+
+  return !!this.cells.filter(function (cell) {
+    return cell !== head && cell.x === head.x && cell.y === head.y
+  }).length;
+};
+
+snake.draw = function () {
+  this.cells.forEach(function (cell) {
+    drawSquare(cell.x, cell.y, 'DodgerBlue', 'white')
+  });
+};
+
+snake.eat = function () {
+  this.length ++;
+  food.setPosition();
+};
+
+snake.initialize = function (startingX, startingY) {
+  this.cells = [];
+  this.direction = 'right';
+  this.length = 5;
+  for (let i = this.length ;i > 0; i--) {
+    this.cells.push({
+      x: startingX + i,
+      y: startingY,
+    });
+  }
+};
+
+snake.moveAndEat = function () {
+  const newHead = {
+    x: this.cells[0].x,
+    y: this.cells[0].y,
+  }
+
+  if (this.direction === 'right') {
+    newHead.x++;
+  } else if (this.direction === 'left') {
+    newHead.x--;
+  } else if (this.direction === 'up') {
+    newHead.y++;
+  } else if (this.direction === 'down') {
+    newHead.y--;
+  }
+
+  if (newHead.x >= c.width / cellSize) {
+    newHead.x = 0;
+  } else if (newHead.x < 0) {
+    newHead.x = c.width / cellSize;
+  } else if (newHead.y >= c.height / cellSize) {
+    newHead.y = 0;
+  } else if (newHead.y < 0) {
+    newHead.y = c.height / cellSize;
+  }
+
+  if (newHead.x === food.x && newHead.y === food.y) {
+    this.eat();
+  } else {
+    this.cells.pop();
+  }
+
+  this.cells.unshift(newHead);
+  return this;
+};
+
+initializeHighScore();
+resetGame();
+setInterval(update, 60);
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode === 40 && snake.direction !== 'down') {
+    snake.direction = 'up';
+  } else if (e.keyCode === 39 && snake.direction !== 'left') {
+    snake.direction = 'right';
+  } else if (e.keyCode === 38 && snake.direction !== 'up') {
+    snake.direction = 'down';
+  } else if (e.keyCode === 37 && snake.direction !== 'right') {
+    snake.direction = 'left';
+  }
+});
+
+function checkHighScore () {
+  if (!localStorage.getItem('highScore')) {
+    highScoreContainer.style.display = 'block';
+  }
+
+  if (!localStorage.getItem('highScore') || localStorage.getItem('highScore') < (snake.length - 5)) {
+    localStorage.setItem('highScore', snake.length - 5);
+    highScore.innerHTML = snake.length - 5;
+  }
+};
+
+function drawScore (fillStyle) {
+  ctx.fillStyle = fillStyle;
+  ctx.fillText('score ' + (snake.length - 5), 5, c.height - 5);
+};
+
+function drawScreen (fillStyle, strokeStyle) {
+  ctx.fillStyle = fillStyle;
+  ctx.fillRect(0, 0, c.width, c.height);
+  ctx.strokeStyle = strokeStyle;
+  ctx.strokeRect(0, 0, c.width, c.height);
+};
+
+function drawSquare (x, y, fillStyle, strokeStyle) {
+  ctx.fillStyle = fillStyle;
+  ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+  ctx.strokeStyle = strokeStyle;
+  ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+};
+
+function initializeHighScore () {
+  if (localStorage.getItem('highScore')) {
+    highScore.innerHTML = localStorage.getItem('highScore');
+    highScoreContainer.style.display = 'block';
+  }
+};
+
+function resetGame () {
+  food.setPosition();
+  snake.initialize(4, 0);
+};
+
+function update () {
+  drawScreen('white', 'black');
+  snake.moveAndEat().draw();
+  food.draw();
+
+  if (snake.checkCollision()) {
+    checkHighScore();
+    resetGame();
+  }
+
+  drawScore('green');
+};
